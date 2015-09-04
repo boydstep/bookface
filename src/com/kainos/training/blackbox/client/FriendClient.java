aabe925f66bf7b278a83cf82bc08f2842d23223a
Stefan Totev <s.totev@kainos.com>
2015-09-04 12:22:04 +0100

    Add duplication check

--- src/com/kainos/training/blackbox/client/FriendClient.java
+++ src/com/kainos/training/blackbox/client/FriendClient.java
@@ -8,8 +8,9 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
-import com.kainos.training.blackboxinterface.model.person.Person;
+import com.kainos.training.blackbox_interface.model.Person;
 
 public class FriendClient {
 
@@ -21,7 +22,17 @@ public class FriendClient {
 	}
 	
 	public Response addFriend(Person p) {
-		Response response = target.request().post(Entity.entity(p, MediaType.APPLICATION_JSON));
+		String name=p.getName();
+		List<Person> actualPeople=getFriendsList();
+		Response response;
+		for(int i=0;i<actualPeople.size();i++)
+		{
+			if(actualPeople.get(i).getName().equals(name)){
+				System.out.println("Name already in use.");
+				return Response.status(Status.CONFLICT).build();
+			}
+		}
+		response = target.request().post(Entity.entity(p, MediaType.APPLICATION_JSON));
 		return response;
 	}
 	
